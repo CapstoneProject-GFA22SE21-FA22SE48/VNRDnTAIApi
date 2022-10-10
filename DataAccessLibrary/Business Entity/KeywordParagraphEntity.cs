f0f24b5a815d76541beaaf464dd0c4f829651276
phucvvt <vovanthanhphuc@gmail.com>
2022-10-10 09:49:29 +0700

    [PhucVVT] Added KeywordParagraph

---
+++ DataAccessLibrary/Business Entity/KeywordParagraphEntity.cs
@@ -0,0 +1,23 @@
+﻿using BusinessObjectLibrary;
+using DataAccessLibrary.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.Business_Entity
+{
+    public class KeywordParagraphBusinessEntity
+    {
+        private IUnitOfWork work;
+        public KeywordParagraphBusinessEntity(IUnitOfWork work)
+        {
+            this.work = work;
+        }
+        public async Task<IEnumerable<KeywordParagraph>> GetKeywordParagraphsByKeywordIdAsync(Guid keywordId)
+        {
+            return (await work.KeywordParagraphs.GetAllAsync())
+                .Where(kp => !kp.IsDeleted && kp.KeywordId == keywordId);
+        }
+    }
+}
