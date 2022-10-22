162c681a654f8b766dd8f1ad8a5ff9056fa9e4cc
JackWH <yukiakira259@gmail.com>
2022-10-22 11:23:59 +0700

    added stuff

--- DataAccessLibrary/Business Entity/QuestionBusinessEntity.cs
+++ DataAccessLibrary/Business Entity/QuestionBusinessEntity.cs
@@ -11,6 +11,7 @@ namespace DataAccessLibrary.Business_Entity
     public class QuestionBusinessEntity
     {
         private IUnitOfWork work;
+        public static Random RandomGen = new Random();
         public QuestionBusinessEntity(IUnitOfWork work)
         {
             this.work = work;
@@ -49,10 +50,37 @@ namespace DataAccessLibrary.Business_Entity
         //***
         public async Task<IEnumerable<Question>> GetRandomTestSetByCategory(string testCatId)
         {
-            var res = (await work.Questions.GetAllAsync(nameof(Question.Answers)))
-                .Where(question => !question.IsDeleted
-                        && question.Status == (int)Status.Active
-                        && question.TestCategoryId.ToString().Equals(testCatId.ToString())).OrderBy(r => Guid.NewGuid()).Take(25);
+            List<Question> res = new List<Question>();
+            double rate = 25 / 7;
+            var qs = (await work.Questions.GetAllAsync(nameof(Question.Answers)))
+                .Where(question => !question.IsDeleted);
+            var qcs = (await work.QuestionCategories.GetAllAsync()).Where(qc => !qc.IsDeleted);
+
+            foreach (var qc in qcs)
+            {
+                if (RandomGen.NextDouble() < (double)(4 / 7))
+                {
+                    rate = Math.Ceiling(rate);
+                }
+                else
+                {
+                    rate = Math.Floor(rate);
+                }
+                res.AddRange(qc.Questions.OrderBy(x => Guid.NewGuid()).Take((int)rate));
+            }
+
+            if (res.Count > 25)
+            {
+                while (res.Count > 25) res.RemoveAt(res.Count - 1);
+            }
+            else if (res.Count < 25)
+            {
+                res.AddRange(qs.Where(q => !q.IsDeleted && !res.Contains(q)).OrderBy(x => Guid.NewGuid()).Take(25 - res.Count));
+            }
+            foreach (var question in res)
+            {
+                question.QuestionCategory = null;
+            }
             return res;
         }
         public async Task<Question> GetQuestionAsync(Guid id)
