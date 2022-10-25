98cc24f1240a174045512d20d111f1cc29e1c454
linh128 <112847909+linh1298@users.noreply.github.com>
2022-10-25 12:27:01 +0700

    Added stuff

---
+++ DTOsLibrary/ManageTasks/TaskDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOsLibrary.ManageTasks
+{
+    public class TaskDTO
+    {
+        public Guid TaskId { get; set; }
+        public string TaskName { get; set; }
+        public Guid? ScribeId { get; set; }
+        public string ScribeName { get; set; }
+        public bool IsAssigned { get; set; }
+
+    }
+}
--- VNRDnTAIApi/Controllers/AssignedColumnsController.cs
+++ VNRDnTAIApi/Controllers/AssignedColumnsController.cs
@@ -1,6 +1,7 @@
 ﻿using BusinessObjectLibrary;
 using DataAccessLibrary.Business_Entity;
 using DataAccessLibrary.Interfaces;
+using DTOsLibrary.ManageTasks;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -45,6 +46,22 @@ namespace VNRDnTAIApi.Controllers
             try
             {
                 return StatusCode(200, await _entity.GetAssignedColumnsByScribeIdAsync(scribeId));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        //-----------------------------------------
+        [HttpGet("Tasks")]
+        [ProducesResponseType(typeof(IEnumerable<TaskDTO>), 200)]
+        [ProducesResponseType(500)]
+        public async Task<ActionResult<IEnumerable<TaskDTO>>> GetTask()
+        {
+            try
+            {
+                return StatusCode(200, await _entity.GetTasksAsync());
             }
             catch (Exception ex)
             {
