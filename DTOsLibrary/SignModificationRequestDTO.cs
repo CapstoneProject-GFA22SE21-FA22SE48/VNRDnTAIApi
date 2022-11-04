18dc6bc8db8607252e19fa997a250a0a0dcaca22
phucvvt <vovanthanhphuc@gmail.com>
2022-11-04 18:51:48 +0700

    [PhucVVT] Added stuff

---
+++ DTOsLibrary/SignModificationRequestDTO.cs
@@ -0,0 +1,33 @@
+﻿using BusinessObjectLibrary;
+using System;
+
+namespace DTOsLibrary
+{
+    public class SignModificationRequestDTO
+    {
+        public Guid Id { get; set; }
+        public Guid? ModifyingSignId { get; set; }
+        public Guid? ModifiedSignId { get; set; }
+        public Guid? ModifyingGpssignId { get; set; }
+        public Guid? ModifiedGpssignId { get; set; }
+        public Guid? UserId { get; set; }
+        public Guid? ScribeId { get; set; }
+        public Guid? AdminId { get; set; }
+        public int OperationType { get; set; }
+        public string ImageUrl { get; set; }
+        public int Status { get; set; }
+        public string DeniedReason { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public virtual User Admin { get; set; }
+        public virtual Gpssign ModifiedGpssign { get; set; }
+        //public virtual Sign ModifiedSign { get; set; }
+        public SignDTO ModifiedSign { get; set; }
+        public virtual Gpssign ModifyingGpssign { get; set; }
+        //public virtual Sign ModifyingSign { get; set; }
+        public SignDTO ModifyingSign { get; set; }
+        public virtual User Scribe { get; set; }
+        public virtual User User { get; set; }
+    }
+}
--- DataAccessLibrary/Business Entity/SignModificationRequestBusinessEntity.cs
+++ DataAccessLibrary/Business Entity/SignModificationRequestBusinessEntity.cs
@@ -1,6 +1,7 @@
 ﻿using BusinessObjectLibrary;
 using BusinessObjectLibrary.Predefined_constants;
 using DataAccessLibrary.Interfaces;
+using DTOsLibrary;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -95,7 +96,7 @@ namespace DataAccessLibrary.Business_Entity
         }
 
         //--------------------------------------------------
-        public async Task<SignModificationRequest> GetSignRomDetail(Guid modifyingSignId)
+        public async Task<SignModificationRequestDTO> GetSignRomDetail(Guid modifyingSignId)
         {
             SignModificationRequest signRom =
                 (await work.SignModificationRequests.GetAllMultiIncludeAsync(
@@ -106,7 +107,279 @@ namespace DataAccessLibrary.Business_Entity
                     .ThenInclude(s => s.SignCategory)
                     ))
                 .Where(s => s.ModifyingSignId == modifyingSignId).FirstOrDefault();
-            return signRom;
+            SignModificationRequestDTO signRomDTO = new SignModificationRequestDTO
+            {
+                Id = signRom.Id,
+                ModifyingSignId = signRom.ModifyingSignId,
+                ModifiedSignId = signRom.ModifiedSignId,
+                //ModifyingGpssignId
+                //ModifiedGpssignId
+                UserId = signRom.UserId,
+                ScribeId = signRom.ScribeId,
+                AdminId = signRom.AdminId,
+                OperationType = signRom.OperationType,
+                ImageUrl = signRom.ImageUrl,
+                Status = signRom.Status,
+                DeniedReason = signRom.DeniedReason,
+                CreatedDate = signRom.CreatedDate,
+                IsDeleted = signRom.IsDeleted,
+
+                Admin = signRom.Admin,
+                //ModifiedGpssign
+                //ModifyingGpssign
+            };
+
+            if (signRom.ModifiedSign != null)
+            {
+                signRomDTO.ModifiedSign = new SignDTO
+                {
+                    Id = signRom.ModifiedSign.Id,
+                    SignCategoryId = signRom.ModifiedSign.SignCategoryId,
+                    Name = signRom.ModifiedSign.Name,
+                    Description = signRom.ModifiedSign.Description,
+                    ImageUrl = signRom.ModifiedSign.ImageUrl,
+                    Status = signRom.ModifiedSign.Status,
+                    IsDeleted = signRom.ModifiedSign.IsDeleted,
+                    SignCategory = signRom.ModifiedSign.SignCategory,
+                };
+
+                var tmpData1 = new List<dynamic>();
+                tmpData1.Add(signRomDTO.ModifiedSign);
+
+                //With paragraph
+                var tmpData2 = from tmp1 in tmpData1
+                               join signParagraph in (await work.SignParagraphs.GetAllAsync())
+                               on tmp1.Id equals signParagraph.SignId
+
+                               join paragraph in (await work.Paragraphs.GetAllAsync())
+                               .Where(p => !p.IsDeleted && p.Status == (int)Status.Active)
+                               on signParagraph.ParagraphId equals paragraph.Id
+                               select new
+                               {
+                                   Id = tmp1.Id,
+                                   SignCategoryId = tmp1.SignCategoryId,
+                                   Name = tmp1.Name,
+                                   Description = tmp1.Description,
+                                   ImageUrl = tmp1.ImageUrl,
+                                   Status = tmp1.Status,
+                                   IsDeleted = tmp1.IsDeleted,
+                                   SignCategory = tmp1.SignCategory,
+
+                                   SignParagraphParagraphId = paragraph.Id,
+                                   SignParagraphParagraphName = paragraph.Name,
+                                   SignParagraphParagraphDesc = paragraph.Description,
+
+                                   SignParagraphSectionId = paragraph.SectionId
+                               };
+                //With section
+                var tmpData3 = from tmp2 in tmpData2
+                               join section in (await work.Sections.GetAllAsync())
+                                .Where(sc => !sc.IsDeleted && sc.Status == (int)Status.Active)
+                                on tmp2.SignParagraphSectionId equals section.Id
+                               select new
+                               {
+                                   Id = tmp2.Id,
+                                   SignCategoryId = tmp2.SignCategoryId,
+                                   Name = tmp2.Name,
+                                   Description = tmp2.Description,
+                                   ImageUrl = tmp2.ImageUrl,
+                                   Status = tmp2.Status,
+                                   IsDeleted = tmp2.IsDeleted,
+                                   SignCategory = tmp2.SignCategory,
+
+                                   SignParagraphParagraphId = tmp2.SignParagraphParagraphId,
+                                   SignParagraphParagraphName = tmp2.SignParagraphParagraphName,
+                                   SignParagraphParagraphDesc = tmp2.SignParagraphParagraphDesc,
+
+                                   SignParagraphSectionId = tmp2.SignParagraphSectionId,
+                                   SignParagraphSectionName = section.Name,
+
+                                   SignParagraphStatueId = section.StatueId
+                               };
+
+                //With statue
+                var tmpData4 = from tmp3 in tmpData3
+                               join statue in (await work.Statues.GetAllAsync())
+                                .Where(st => !st.IsDeleted && st.Status == (int)Status.Active)
+                                on tmp3.SignParagraphStatueId equals statue.Id
+                               select new
+                               {
+                                   Id = tmp3.Id,
+                                   SignCategoryId = tmp3.SignCategoryId,
+                                   Name = tmp3.Name,
+                                   Description = tmp3.Description,
+                                   ImageUrl = tmp3.ImageUrl,
+                                   Status = tmp3.Status,
+                                   IsDeleted = tmp3.IsDeleted,
+                                   SignCategory = tmp3.SignCategory,
+
+                                   SignParagraphParagraphId = tmp3.SignParagraphParagraphId,
+                                   SignParagraphParagraphName = tmp3.SignParagraphParagraphName,
+                                   SignParagraphParagraphDesc = tmp3.SignParagraphParagraphDesc,
+
+                                   SignParagraphSectionId = tmp3.SignParagraphSectionId,
+                                   SignParagraphSectionName = tmp3.SignParagraphSectionName,
+
+                                   SignParagraphStatueId = tmp3.SignParagraphStatueId,
+                                   SignParagraphStatueName = statue.Name
+                               };
+                List<SignParagraphDTO> signParagraphList = null;
+
+                foreach (var sign in tmpData1)
+                {
+                    signParagraphList = new List<SignParagraphDTO>();
+                    foreach (var data in tmpData4)
+                    {
+                        if (data.Id == sign.Id)
+                        {
+                            signParagraphList.Add(new SignParagraphDTO
+                            {
+                                SignParagraphParagraphId = data.SignParagraphParagraphId,
+                                SignParagraphParagraphName = data.SignParagraphParagraphName,
+                                SignParagraphParagraphDesc = data.SignParagraphParagraphDesc,
+
+                                SignParagraphSectionId = data.SignParagraphSectionId,
+                                SignParagraphSectionName = data.SignParagraphSectionName,
+
+                                SignParagraphStatueId = data.SignParagraphStatueId,
+                                SignParagraphStatueName = data.SignParagraphStatueName
+                            });
+                        }
+                    }
+                }
+                signParagraphList.OrderBy(r => int.Parse(r.SignParagraphStatueName.Split(" ")[1]))
+                        .ThenBy(r => int.Parse(r.SignParagraphSectionName.Split(" ")[1]))
+                        .ThenBy(r => r.SignParagraphParagraphName).ToList();
+                signRomDTO.ModifiedSign.SignParagraphs = signParagraphList;
+                signRomDTO.ModifiedSign.SignCategory = null;
+            }
+
+            if (signRom.ModifyingSign != null)
+            {
+                signRomDTO.ModifyingSign = new SignDTO
+                {
+                    Id = signRom.ModifyingSign.Id,
+                    SignCategoryId = signRom.ModifyingSign.SignCategoryId,
+                    Name = signRom.ModifyingSign.Name,
+                    Description = signRom.ModifyingSign.Description,
+                    ImageUrl = signRom.ModifyingSign.ImageUrl,
+                    Status = signRom.ModifyingSign.Status,
+                    IsDeleted = signRom.ModifyingSign.IsDeleted,
+                    SignCategory = signRom.ModifyingSign.SignCategory,
+                };
+
+                var tmpData1 = new List<dynamic>();
+                tmpData1.Add(signRomDTO.ModifyingSign);
+
+                //With paragraph
+                var tmpData2 = from tmp1 in tmpData1
+                               join signParagraph in (await work.SignParagraphs.GetAllAsync())
+                               on tmp1.Id equals signParagraph.SignId
+
+                               join paragraph in (await work.Paragraphs.GetAllAsync())
+                               .Where(p => !p.IsDeleted && p.Status == (int)Status.Active)
+                               on signParagraph.ParagraphId equals paragraph.Id
+                               select new
+                               {
+                                   Id = tmp1.Id,
+                                   SignCategoryId = tmp1.SignCategoryId,
+                                   Name = tmp1.Name,
+                                   Description = tmp1.Description,
+                                   ImageUrl = tmp1.ImageUrl,
+                                   Status = tmp1.Status,
+                                   IsDeleted = tmp1.IsDeleted,
+                                   SignCategory = tmp1.SignCategory,
+
+                                   SignParagraphParagraphId = paragraph.Id,
+                                   SignParagraphParagraphName = paragraph.Name,
+                                   SignParagraphParagraphDesc = paragraph.Description,
+
+                                   SignParagraphSectionId = paragraph.SectionId
+                               };
+                //With section
+                var tmpData3 = from tmp2 in tmpData2
+                               join section in (await work.Sections.GetAllAsync())
+                                .Where(sc => !sc.IsDeleted && sc.Status == (int)Status.Active)
+                                on tmp2.SignParagraphSectionId equals section.Id
+                               select new
+                               {
+                                   Id = tmp2.Id,
+                                   SignCategoryId = tmp2.SignCategoryId,
+                                   Name = tmp2.Name,
+                                   Description = tmp2.Description,
+                                   ImageUrl = tmp2.ImageUrl,
+                                   Status = tmp2.Status,
+                                   IsDeleted = tmp2.IsDeleted,
+                                   SignCategory = tmp2.SignCategory,
+
+                                   SignParagraphParagraphId = tmp2.SignParagraphParagraphId,
+                                   SignParagraphParagraphName = tmp2.SignParagraphParagraphName,
+                                   SignParagraphParagraphDesc = tmp2.SignParagraphParagraphDesc,
+
+                                   SignParagraphSectionId = tmp2.SignParagraphSectionId,
+                                   SignParagraphSectionName = section.Name,
+
+                                   SignParagraphStatueId = section.StatueId
+                               };
+
+                //With statue
+                var tmpData4 = from tmp3 in tmpData3
+                               join statue in (await work.Statues.GetAllAsync())
+                                .Where(st => !st.IsDeleted && st.Status == (int)Status.Active)
+                                on tmp3.SignParagraphStatueId equals statue.Id
+                               select new
+                               {
+                                   Id = tmp3.Id,
+                                   SignCategoryId = tmp3.SignCategoryId,
+                                   Name = tmp3.Name,
+                                   Description = tmp3.Description,
+                                   ImageUrl = tmp3.ImageUrl,
+                                   Status = tmp3.Status,
+                                   IsDeleted = tmp3.IsDeleted,
+                                   SignCategory = tmp3.SignCategory,
+
+                                   SignParagraphParagraphId = tmp3.SignParagraphParagraphId,
+                                   SignParagraphParagraphName = tmp3.SignParagraphParagraphName,
+                                   SignParagraphParagraphDesc = tmp3.SignParagraphParagraphDesc,
+
+                                   SignParagraphSectionId = tmp3.SignParagraphSectionId,
+                                   SignParagraphSectionName = tmp3.SignParagraphSectionName,
+
+                                   SignParagraphStatueId = tmp3.SignParagraphStatueId,
+                                   SignParagraphStatueName = statue.Name
+                               };
+                List<SignParagraphDTO> signParagraphList = null;
+
+                foreach (var sign in tmpData1)
+                {
+                    signParagraphList = new List<SignParagraphDTO>();
+                    foreach (var data in tmpData4)
+                    {
+                        if (data.Id == sign.Id)
+                        {
+                            signParagraphList.Add(new SignParagraphDTO
+                            {
+                                SignParagraphParagraphId = data.SignParagraphParagraphId,
+                                SignParagraphParagraphName = data.SignParagraphParagraphName,
+                                SignParagraphParagraphDesc = data.SignParagraphParagraphDesc,
+
+                                SignParagraphSectionId = data.SignParagraphSectionId,
+                                SignParagraphSectionName = data.SignParagraphSectionName,
+
+                                SignParagraphStatueId = data.SignParagraphStatueId,
+                                SignParagraphStatueName = data.SignParagraphStatueName
+                            });
+                        }
+                    }
+                }
+                signParagraphList.OrderBy(r => int.Parse(r.SignParagraphStatueName.Split(" ")[1]))
+                        .ThenBy(r => int.Parse(r.SignParagraphSectionName.Split(" ")[1]))
+                        .ThenBy(r => r.SignParagraphParagraphName).ToList();
+                signRomDTO.ModifyingSign.SignParagraphs = signParagraphList;
+                signRomDTO.ModifyingSign.SignCategory = null;
+            }
+
+            return signRomDTO;
         }
         //--------------------------------------------------
         public async Task<SignModificationRequest> GetGpssignRomDetail(Guid modifyingGpssignId)
