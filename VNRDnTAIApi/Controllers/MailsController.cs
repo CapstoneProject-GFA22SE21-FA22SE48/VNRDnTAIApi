6c3d0db93e47c6cf211e9cf1cb769b6456ebc1d7
phucvvt <vovanthanhphuc@gmail.com>
2022-11-15 16:17:06 +0700

    [PhucVVT] Added retrain AI

---
+++ VNRDnTAIApi/Controllers/MailsController.cs
@@ -0,0 +1,36 @@
+﻿using DataAccessLibrary.Business_Entity;
+using DataAccessLibrary.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VNRDnTAIApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    public class MailsController : ControllerBase
+    {
+        private readonly MailBusinessEntity _entity;
+
+        public MailsController(IConfiguration configuration)
+        {
+            _entity = new MailBusinessEntity(configuration);
+        }
+
+        //POST: api/Mails
+        [HttpPost]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(500)]
+        public IActionResult SendRetrainEmail([FromBody] string body)
+        {
+            try
+            {
+               _entity.SendRetrainEmail(body);
+               return StatusCode(200);
+            } catch(Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
